97313e24893f4a32f9269795340187954f6fc73f
nanbowan123 <33351427+nanbowan123@users.noreply.github.com>
2025-03-01 22:25:26 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdaaaa.com"];
-pub const RS_PUB_KEY: &str = "123456789";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.cdcardzn.net"];
+pub const RS_PUB_KEY: &str = "";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
